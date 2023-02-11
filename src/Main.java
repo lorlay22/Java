c864fecb86704692cbc9c5dc896851ac6798f658
lorlay22 <84510863+lorlay22@users.noreply.github.com>
2023-02-10 17:27:21 -1100

    TP2

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        discriminant();
+    }
+
+    public static void discriminant() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Quelle est la valeur de a ?");
+        int a = scanner.nextInt();
+        System.out.println("Quelle est la valeur de b ?");
+        int b = scanner.nextInt();
+        System.out.println("Quelle est la valeur de c ?");
+        int c = scanner.nextInt();
+        int delta = (int) (Math.pow(b, 2) - 4 * a * c);
+        if (delta < 0) {
+            float delta5=(-b -(float) (Math.sqrt(-delta)))/(2*a);
+            float delta6=(-b +(float) (Math.sqrt(-delta)))/(2*a);
+            System.out.println("Ce polynome a 2 racines complexes i"+delta5+ " et i"+delta6);
+        }else if ( delta == 0) {
+            int delta2=(-b/2*a);
+            System.out.println("Ce polynome a une racine double" +delta2 );
+        }else {
+            float delta3= (-b +(float) (Math.sqrt(delta)))/(2*a);
+            float delta4= (-b-(float)(Math.sqrt(delta)))/(2*a);
+            System.out.println("Ce polynome a 2 racines " +delta3+ " et" +delta4 );
+
+        }
+    }
+
+}
